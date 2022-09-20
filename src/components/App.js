0800cad5130a025c9e3e37f64d08ba645b556989
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-09-20 18:52:30 +0300

    completed assgnment

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,17 @@ function Home() {
   );
 }
 
+
+
+
 {/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+      </div>
+  );
+}
 
 
 function App() {
@@ -25,6 +35,9 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home/>
+      <About/>
+      
     </div>
   );
 }
